a3e6e2d7c92d5b61cfe721a7335eeb23ff6042d2
Andrei <barbutodev@gmail.com>
2024-10-17 22:30:30 -0300

    estrutura das pagias

--- src/app/page.js
+++ src/app/page.js
@@ -18,14 +18,15 @@ export default function Home() {
 
 
 
-        <img className={styles.mDesk} src="img/menuDesk.png" />
-        <img className={styles.palavraMenu} src="img/palavraMenu.png" />
-        <img className={styles.mMobile} src="img/menu.png" />
-        <Link  href="/leitura">
-          <img className={styles.livroAkin} src="img/livroAkin.png" />
+      
+      <Image  width={100} height={100}className={styles.mDesk} src="/img/menuDesk.png" alt="Menu Desk" quality={100} />
+        <Image   width={100} height={100} className={styles.palavraMenu} src="/img/palavraMenu.png" alt="Palavra Menu" quality={100} />
+        <Image   width={100} height={100} className={styles.mMobile} src="/img/menu.png" alt="Menu Mobile" quality={100} />
+        <Link href="/leitura">
+          <Image   width={100} height={100} className={styles.livroAkin} src="/img/livroAkin.png" alt="Livro Akin" quality={100} />
         </Link>
-        <img className={styles.homenzin} src="img/homenzin.png" />
-        <img className={styles.saco} src="img/saco.png" />
+        <Image   width={100} height={100} className={styles.homenzin} src="/img/homenzin.png" alt="Homenzinho" quality={100} />
+        <Image   width={100} height={100} className={styles.saco} src="/img/saco.png" alt="Saco" quality={100} />
 
         {/* <h1 className="emBreve">EM BREVE</h1> */}
 
